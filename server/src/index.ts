cd77606c9fd43b8eaa705eb2d3ebe2e35aacb72c
SkyDogzz <thomas.stephan@live.fr>
2023-11-16 16:56:57 +0000

    Add PostgreSQL and Sequelize dependencies, update target and module in tsconfig.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,22 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("marketpulse", "postgres", "yourpassword", {
+  host: "db",
+  port: 5432,
+  dialect: "postgres",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+    sequelize.sync();
+  })
+  .catch((err: any) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const app = express();
 const port = process.env.PORT || 5000;
