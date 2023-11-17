2685dfb2e087740c08fbc2daf183f1a7f66217e1
SkyDogzz <thomas.stephan@live.fr>
2023-11-17 13:28:08 +0000

    Add register, update, and delete routes for users

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -57,4 +57,70 @@ router.post("/login", (req, res) => {
     });
 });
 
+router.post("/register", (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  User.create({ email, password, firstName, lastName })
+    .then((user) => {
+      res.json({
+        status: {
+          code: 200,
+          message: "Success",
+        },
+        user,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { email, password, firstName, lastName } = req.body;
+  User.update({ email, password, firstName, lastName }, { where: { id } })
+    .then(() => {
+      return User.findByPk(id);
+    })
+    .then((user) => {
+      res.json({
+        status: {
+          code: 200,
+          message: "Success",
+        },
+        user,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  User.findByPk(id).then((user) => {
+    User.destroy({ where: { id } })
+      .then(() => {
+        res.json({
+          status: {
+            code: 200,
+            message: "Success",
+          },
+          user,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({ status: { code: 500, message: "Internal server error" } });
+      });
+  });
+});
+
 export default router;
