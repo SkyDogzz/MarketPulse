1942d9a28cb61161004aede3eac4b3f6e997e0f1
SkyDogzz <thomas.stephan@live.fr>
2023-11-17 12:18:05 +0000

    Add CRUD routes for posts

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -22,4 +22,112 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Post.findByPk(id)
+    .then((post) => {
+      if (post) {
+        res.json({
+          status: {
+            code: 200,
+            message: "Success",
+          },
+          post,
+        });
+      } else {
+        res.json({
+          status: {
+            code: 404,
+            message: "Not found",
+          },
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
+router.post("/", (req, res) => {
+  const { userId, title, body } = req.body;
+  Post.create({ userId, title, body })
+    .then((post) => {
+      res.json({
+        status: {
+          code: 200,
+          message: "Success",
+        },
+        post,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { userId, title, body } = req.body;
+  Post.update({ userId, title, body }, { where: { id } })
+    .then((post) => {
+      if (post) {
+        res.json({
+          status: {
+            code: 200,
+            message: "Success",
+          },
+          post,
+        });
+      } else {
+        res.json({
+          status: {
+            code: 404,
+            message: "Not found",
+          },
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Post.destroy({ where: { id } })
+    .then((post) => {
+      if (post) {
+        res.json({
+          status: {
+            code: 200,
+            message: "Success",
+          },
+          post,
+        });
+      } else {
+        res.json({
+          status: {
+            code: 404,
+            message: "Not found",
+          },
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ status: { code: 500, message: "Internal server error" } });
+    });
+});
+
 export default router;
