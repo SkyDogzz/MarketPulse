721217f8834ecb58c27794a51abb7d13f88997d2
SkyDogzz <thomas.stephan@live.fr>
2023-11-18 18:38:03 +0000

    Add link to product details page in ProductCard component

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,10 +1,13 @@
 import { Product } from "../types";
+import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
-    <li className="product-card">
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-    </li>
+    <Link to={`/products/${product.id}`}>
+      <li className="product-card">
+        <h3>{product.name}</h3>
+        <p>{product.description}</p>
+      </li>
+    </Link>
   );
 }
