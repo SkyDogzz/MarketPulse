f7a57930c3f349a212147e5368e5eae03cf3cc40
SkyDogzz <thomas.stephan@live.fr>
2023-11-18 18:16:17 +0000

    Add product listing on home page

--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -1,19 +1,35 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+};
 
 export default function Home() {
-  const apiuRL = import.meta.env.VITE_API_URL;
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    axios.get(`${apiuRL}/products`).then((res) => {
-      console.log(res.data);
+    const apiURL = import.meta.env.VITE_API_URL;
+    axios.get(`${apiURL}/products/last/4`).then((res) => {
+      setProducts(res.data.products);
     });
-  }, [apiuRL]);
-  
+  }, []);
 
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+      <h2>Les 4 derniers produits ajoutés :</h2>
+      {products !== null && products.length > 0 ? (
+        products.map((product: Product) => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No products available</p>
+      )}
     </div>
   );
 }
