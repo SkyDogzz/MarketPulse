45b6bd82c2f5e17cba0d03d624420930e02bd889
SkyDogzz <thomas.stephan@live.fr>
2023-11-19 17:00:10 +0000

    Add footer component to the project

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p> 2023 Marketplace. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 }
