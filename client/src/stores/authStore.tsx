b38d552cac6b2875e2fe991195d16ae3bc1dee63
SkyDogzz <thomas.stephan@live.fr>
2023-11-19 13:52:06 +0000

    Refactor authStore.tsx to use localStorage for token and user

--- client/src/stores/authStore.tsx
+++ client/src/stores/authStore.tsx
@@ -1,5 +1,6 @@
-import {create} from 'zustand'
-import {User} from "../types";
+
+import { create } from 'zustand';
+import { User } from "../types";
 
 type AuthStore = {
   token: string | null;
@@ -8,11 +9,24 @@ type AuthStore = {
   clearAuth: () => void;
 };
 
-const useAuthStore = create<AuthStore>(set => ({
-  token: null,
-  user: null, 
-  setAuth: (token: string, user: User) => set({ token, user }),
-  clearAuth: () => set({ token: null, user: null })
-}));
+const useAuthStore = create<AuthStore>(set => {
+  const storedToken = localStorage.getItem('token');
+  const storedUser = localStorage.getItem('user');
+
+  return {
+    token: storedToken,
+    user: storedUser ? JSON.parse(storedUser) : null,
+    setAuth: (token: string, user: User) => {
+      localStorage.setItem('token', token);
+      localStorage.setItem('user', JSON.stringify(user));
+      set({ token, user });
+    },
+    clearAuth: () => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      set({ token: null, user: null });
+    }
+  };
+});
 
 export default useAuthStore;
