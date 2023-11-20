3b4e0f718ed6c35067bcd77f19a406bcd30ee448
SkyDogzz <thomas.stephan@live.fr>
2023-11-20 09:56:59 +0000

    Add createStripeUser function to populate.ts

--- server/src/populate.ts
+++ server/src/populate.ts
@@ -1,36 +1,7 @@
 import fs from "fs";
 import sequelize from "./config/database";
 import bcrypt from "bcrypt";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-type User = {
-  email: string;
-  firstName: string;
-  lastName: string;
-};
-
-async function createStripeUser(user: User) {
-  try {
-    const existingUser = await stripe.customers.list({
-      email: user.email,
-    });
-
-    if (existingUser.data.length > 0) {
-      return existingUser.data[0];
-    }
-
-    const stripeCustomer = await stripe.customers.create({
-      email: user.email,
-      name: user.firstName + " " + user.lastName,
-      description: "Marketplace user",
-    });
-
-    return stripeCustomer;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
+import { createStripeUser } from "./services/stripe";
 
 export default function populate() {
   console.log("Populating database...");
---
+++ server/src/services/stripe.ts
@@ -0,0 +1,30 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+type User = {
+  email: string;
+  firstName: string;
+  lastName: string;
+};
+
+export async function createStripeUser(user: User) {
+  try {
+    const existingUser = await stripe.customers.list({
+      email: user.email,
+    });
+
+    if (existingUser.data.length > 0) {
+      return existingUser.data[0];
+    }
+
+    const stripeCustomer = await stripe.customers.create({
+      email: user.email,
+      name: user.firstName + " " + user.lastName,
+      description: "Marketplace user",
+    });
+
+    return stripeCustomer;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
