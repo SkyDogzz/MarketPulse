bb7301641b3c6178e61929a5cad2f13a09052f9d
SkyDogzz <thomas.stephan@live.fr>
2023-11-20 08:45:32 +0000

    Add user profile card component

--- client/src/page/UserProfile.tsx
+++ client/src/page/UserProfile.tsx
@@ -6,20 +6,24 @@ export default function UserProfile() {
   const navigate = useNavigate();
   const user = useAuthStore((state) => state.user);
 
+  const img = "https://picsum.photos/seed/" + user?.id + "/200/200";
+
   useEffect(() => {
     if (!user) {
       navigate("/login");
     }
-  })
+  });
 
   return (
-    <div>
-      <h1>User Profile</h1>
-      <p>Email: {user?.email}</p>
-      <p>FirstName: {user?.firstName}</p>
-      <p>LastName: {user?.lastName}</p>
-      <p>CreatedAt: {user?.createdAt}</p>
-      <p>UpdatedAt: {user?.updatedAt}</p>
+    <div className="user-card">
+      <h1 className="user-card-title">User Profile</h1>
+      <img className="user-card-img" src={img} alt="Photo de profil" />
+      <p className="user-card-detail">Email: <span className="user-card-value">{user?.email}</span></p>
+      <p className="user-card-detail">FirstName: <span className="user-card-value">{user?.firstName}</span></p>
+      <p className="user-card-detail">LastName: <span className="user-card-value">{user?.lastName}</span></p>
+      <p className="user-card-detail">CreatedAt: <span className="user-card-value">{user?.createdAt}</span></p>
+      <p className="user-card-detail">UpdatedAt: <span className="user-card-value">{user?.updatedAt}</span></p>
     </div>
   );
+  
 }
